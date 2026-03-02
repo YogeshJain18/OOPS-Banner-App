3bc59ada978b2fa6532d7f784eb2a6b94c3fe2a2
YogeshJain18 <yogesh2006jain@gmail.com>
2026-03-02 22:17:36 +0530

    Update OOPSBannerAppUC2.java

--- OOPSBannerAppUC2.java
+++ OOPSBannerAppUC2.java
@@ -1,6 +1,5 @@
 public class OOPSBannerApp {
-    
-    // Banner data as a class constant
+ 
     private static final String[] BANNER_LINES = {
         "  *****    *****   *****    *****  ",
         " *     *  *     *  *    *  *     * ",
@@ -12,15 +11,15 @@ public class OOPSBannerApp {
     };
     
     public static void main(String[] args) {
-        // Create an instance and display banner
+    
         OOPSBannerApp app = new OOPSBannerApp();
         app.displayBanner();
     }
-    
-    // Method to display the banner
+
     public void displayBanner() {
         for (String line : BANNER_LINES) {
             System.out.println(line);
         }
     }
+
 }
