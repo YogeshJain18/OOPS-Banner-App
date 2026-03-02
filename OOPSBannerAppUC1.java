9a8720bf0bdaf138fd394676641667b5cee60815
YogeshJain18 <yogesh2006jain@gmail.com>
2026-03-02 22:16:59 +0530

    Update OOPSBannerAppUC1.java

--- OOPSBannerAppUC1.java
+++ OOPSBannerAppUC1.java
@@ -1,11 +1,9 @@
 public class OOPSBannerApp {
 
-    /**
-     * Main method - Entry point of the application
-     */
+   
     public static void main(String[] args) {
 
-        // Printing the banner text
         System.out.println("OOPS");
     }
+
 }
